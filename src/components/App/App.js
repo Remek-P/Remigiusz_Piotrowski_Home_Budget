518027baa2c30f2327c248ed4b293c007d4cf425
Remek-P <remek.piotrowski@wp.pl>
2022-10-18 15:48:04 +0200

    Main Configuration - styling(variables)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Overview } from "../Category/Overview";
+import { Cat } from "../Category/Category";
+
 
 export function App() {
-    return "Hello APP"
+    return (
+        <div className={"main"}>
+            <h1 className={"main__header"}>Your Budget</h1>
+            <Overview />
+            <Cat />
+            <Cat />
+        </div>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App} from "./components/App/App";
+import { App } from "./components/App/App";
 import "./main.scss"
-import "./style.css"
 
 const container = document.getElementById("app");
 const root = createRoot(container);
