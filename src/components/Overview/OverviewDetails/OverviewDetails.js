59334c0db451677d9b57a323a2d0dae588c48638
Remek-P <remek.piotrowski@wp.pl>
2022-11-04 15:19:16 +0100

    Overview styling prepared

--- src/components/Overview/OverviewDetails/OverviewDetails.js
+++ src/components/Overview/OverviewDetails/OverviewDetails.js
@@ -3,104 +3,41 @@ import React from "react";
 export function OverviewDetails() {
     return (
             <section className={"overview-details"}>
-                <h3>All expenses</h3>
+                <h3 className={"overview-details__header"}>All expenses</h3>
                 <ul className={"overview-details-summup"}>
                     <li>
-                        <h5>THIS MONTH</h5>
-                        <div>$12,536</div>
+                        <h5 className={"overview-details-summup__header"}>This month
+                            <div className={"overview-details-summup__values"}>$12,536</div>
+                        </h5>
+
                     </li>
                     <li>
-                        <h5>PREV. MONTH</h5>
-                        <div>$15,000</div>
+                        <h5 className={"overview-details-summup__header"}>Prev. month
+                            <div className={"overview-details-summup__values"}>$15,000</div>
+                        </h5>
                     </li>
                     <li>
-                        <h5>M2M CHANGE</h5>
-                        <div>+13%</div>
+                        <h5 className={"overview-details-summup__header"}>M2M&nbsp;change
+                            <div className={"overview-details-summup__values"}>+13%</div>
+                        </h5>
                     </li>
                 </ul>
                 <ul>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
+                    <li className={"overview-details-expense"}>
+                        <h5 className={"overview-details-expense__header"}>Today, 16 October 2022</h5>
+                        <article className={"overview-details-expense__container"}>
+                            <h4 className={"overview-details-expense__container__header"}>Expense 18</h4>
+                            <div className={"overview-details-expense__container__value"}>$500</div>
+                        </article>
                     </li>
-                    <li>
-                       <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
+                    <li className={"overview-details-expense"}>
+                        <h5 className={"overview-details-expense__header"}>Today, 16 October 2022</h5>
+                        <article className={"overview-details-expense__container"}>
+                            <h4 className={"overview-details-expense__container__header"}>Expense 18</h4>
+                            <div className={"overview-details-expense__container__value"}>$500</div>
+                        </article>
                     </li>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                    <li>
-                       <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                    <li>
-                       <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
-                    <li>
-                        <div>Today, 16 October 2022</div>
-                        <div>Expense 18</div>
-                        <div>$500</div>
-                    </li>
+
                 </ul>
             </section>
     )
